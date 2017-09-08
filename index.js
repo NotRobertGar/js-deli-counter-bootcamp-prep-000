d8b60e53fb7926b3c7abd56066df12f7ce9f8ce6
cab driver <cocktailsthecomic@gmail.com>
2017-09-08 23:16:11 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,7 +7,9 @@ return (`Welcome, ${name}. You are number ${katzDeliLine.length} in line.`);
 }
 
 function nowServing(katzDeliLine){
-  if (katzDeliLine === []) {
+  
+  if (katzDeliLine.length === 0) {
     return "There is nobody waiting to be served!";
   }
+  else if (katzDeliLine )
 }
