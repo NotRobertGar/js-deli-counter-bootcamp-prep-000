33c9072c070eb4c8e0abaf11e06aa9d596190563
cab driver <cocktailsthecomic@gmail.com>
2017-09-09 04:08:13 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ return (`Welcome, ${name}. You are number ${katzDeliLine.length} in line.`);
 function nowServing(katzDeliLine){
   for (var i = 0; i < katzDeliLine.length; i++;)
   if (katzDeliLine > 0){
-    
+    return (`Currently serving ${katzDeli.shift()}.`);    
   }
   if (katzDeliLine.length === 0) {
     return "There is nobody waiting to be served!";
