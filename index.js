989cf2ab6dc7656f0b17f5cfd38058582ed4223e
cab driver <cocktailsthecomic@gmail.com>
2017-09-09 04:23:15 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ function currentLine(katzDeliLine){
   if (katzDeliLine.length > 0 ){
       var line = [];
   for (var i = 0; i < katzDeliLine.length; i++){
-    line.push((i+1) + '. ' + katzDeliLine[i]);
+    line.push(' ' + (i+1) + '. ' + katzDeliLine[i]);
   }
   return ("The line is currently: " + line);
 }
