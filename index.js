1d6a7fa6fcfce360ac550f20b2e9ea382183da27
cab driver <cocktailsthecomic@gmail.com>
2017-09-09 04:21:04 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -17,11 +17,10 @@ function nowServing(katzDeliLine){
 }
 
 function currentLine(katzDeliLine){
-  var line = {};
   if (katzDeliLine.length === 0){
     return "The line is currently empty."
   }
-  if (katzDeliLine.length === 0 ){
+  if (katzDeliLine.length > 0 ){
   for (var i = 0; i < katzDeliLine.length; i++){
     line.push([i+1] + '. ' + katzDeliLine[i]);
   }
@@ -29,3 +28,16 @@ function currentLine(katzDeliLine){
 }
 
 }
+
+
+function currentLine(katzDeliLine){
+  // if line is empty, announce line is empty
+  if (katzDeliLine.length === 0){
+    return "The line is currently empty.";
+  } else {
+    var current_line = [];
+    for (var i = 0; i<katzDeliLine.length; i++){
+      current_line.push((i+1)+". "+katzDeliLine[i]);
+    }
+    return "The line is currently: " + current_line.join(", ");
+  }
